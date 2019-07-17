506c2f78a9e1299e2278d0e81e6d22b70ba90a64
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2019-07-17 23:15:17 +0900

    Vue: テンプレート構文を完成

--- 20190711-pt11-vuejs-direct-sample/vuesample-3.js
+++ 20190711-pt11-vuejs-direct-sample/vuesample-3.js
@@ -2,12 +2,38 @@ new Vue({
   el: "#app1",
   data: {
     onceMsg: "v-onceで設定されたメッセージ",
-    notOnceMsg: "v-onceで設定されていないメッセージ"
+    notOnceMsg: "v-onceで設定されていないメッセージ",
+    htmlMessage: "<span style=\"font-size: 300%\">HTMLを許しちゃう</span>",
+    emClass: "",
+    isRadioDisabled: true,
+    isJavaScriptSectionDisplayed: true,
+    vueImageSrc: "https://jp.vuejs.org/images/logo.png",
+    imgWidth: 150,
+    toggle_event_name: "click",
+    imgTitle: "Vue.jsのロゴ"
   },
   methods: {
     changeMessage: function() {
       this.onceMsg = "hoge";
       this.notOnceMsg = "hoge";
+    },
+    changeEmphasize: function() {
+      if (this.isRadioDisabled) {
+        this.isRadioDisabled = false;
+        this.emClass = "redbold";
+      } else {
+        this.isRadioDisabled = true;
+        this.emClass = "";
+      }
+    },
+    changeToggleMode: function() {
+      if (this.toggle_event_name === "click") {
+        this.toggle_event_name = "mouseover";
+      } else {
+        this.toggle_event_name = "click";
+      }
+
+      this.changeEmphasize();
     }
   }
 })
