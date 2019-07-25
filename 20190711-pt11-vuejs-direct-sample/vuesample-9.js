535ab68804348ecb104f4b3e098bde9f4ca33319
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2019-07-26 01:50:26 +0900

    Vue: フォーム入力バインディング完成

--- 20190711-pt11-vuejs-direct-sample/vuesample-9.js
+++ 20190711-pt11-vuejs-direct-sample/vuesample-9.js
@@ -1,19 +1,35 @@
 new Vue({
-  el: "#app1",
+  el: '#app1',
   data: {
-    message: "1行だけ",
-    multiLineMessage: "複数行\nですね~",
+    message: '1行だけ',
+    multiLineMessage: '複数行\nですね~',
     checked: [false, true],
     selectedNames: [],
-    radioTuned: "",
-    selectedRadio: "",
+    radioTuned: '',
+    selectedRadio: '',
     favoriteRadioStation: [],
     featuredRadio: [
-      { text: "70.0MHz D radio", value: "D" },
-      { text: "77.0MHz E radio", value: "E" },
-      { text: "82.0MHz F radio", value: "F" },
-      { text: "86.0MHz G radio", value: "G" }
+      { text: '70.0MHz D radio', stationName: 'D radio', value: 'D' },
+      { text: '77.0MHz E radio', stationName: 'E radio', value: 'E' },
+      { text: '82.0MHz F radio', stationName: 'F radio', value: 'F' },
+      { text: '86.0MHz G radio', stationName: 'G radio', value: 'G' }
     ],
-    selectedFeaturedRadio: []
+    selectedFeaturedRadio: [],
+    bindedSelection: ['v-bindで', 'valueを変更できます'],
+    selectedBindMessage: '',
+    selectedCheckMessage: '',
+    selectedPickMessage: '',
+    selectedSelectObject: {},
+    lazyInputedText: '',
+    notCastedValue: '',
+    castedValue: '',
+    notTrimedInputValue: '',
+    trimedInputValue: ''
+  },
+  methods: {
+    selectChange: function() {
+      alert('typeof: ' + typeof this.selectedSelectObject);
+      alert(this.selectedSelectObject['stationName']);
+    }
   }
 });
