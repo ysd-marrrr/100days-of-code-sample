7e235236107a51b20df6d7a12ce53b1d47db1fff
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2019-07-31 23:59:41 +0900

    Vue: コンポーネントの基本完成

--- 20190711-pt11-vuejs-direct-sample/vuesample-10.js
+++ 20190711-pt11-vuejs-direct-sample/vuesample-10.js
@@ -43,13 +43,38 @@ Vue.component("articles-count", {
 });
 
 Vue.component("follow-up-tool", {
-  props: ['amount'],
-  template: '<div><button @click="$emit(\'count-up-amount\', amount)"> + {{ amount }} </button></div>'
+  props: ["amount"],
+  template:
+    "<div><button @click=\"$emit('count-up-amount', amount)\"> + {{ amount }} </button></div>"
 });
 
 Vue.component("custom-input", {
-  props: ['inputtedValue'],
+  props: ["inputtedValue"],
   template: `<input :value="inputtedValue" @input="$emit('onCustomInput', $event.target.value)"></input>`
+});
+
+// 6. スロットによるコンテンツ配信
+Vue.component("custom-message", {
+  props: ["title"],
+  template: `<div style="border-radius: 5px; border: 1px dashed gray; background-color: white; box-shadow: 5px 5px 5px  gray"><h3>{{ title }}</h3><slot></slot></div> `
+});
+
+// 7. 動的なコンポーネント
+Vue.component("first-tab", {
+  template:
+    "<p>1st tab content<br>1st tab content<br>1st tab content<br>1st tab content</p>"
+});
+Vue.component("second-tab", {
+  template:
+    "<p>2nd tab content<br>2nd tab content<br>2nd tab content<br>2nd tab content</p>"
+});
+Vue.component("third-tab", {
+  template:
+    "<p>3rd tab content<br>3rd tab content<br>3rd tab content<br>3rd tab content</p>"
+});
+Vue.component("fourth-tab", {
+  template:
+    "<p>4th tab content<br>4th tab content<br>4th tab content<br>4th tab content</p>"
 });
 
 new Vue({ el: "#components-1" });
@@ -76,15 +101,40 @@ new Vue({
   data: {
     articles: sampleArticles,
     clickCount: 0,
-    censoredMessage: 'ここに検閲された入力値が表示されます'
+    censoredMessage: "ここに検閲された入力値が表示されます"
   },
   methods: {
-    onCountUp: function (countupAmount) {
+    onCountUp: function(countupAmount) {
       this.clickCount += countupAmount;
     },
-    onCustomInput: function (inputtedValue) {
-      console.log('inputted');
-      this.censoredMessage = "censored"
+    onCustomInput: function(inputtedValue) {
+      console.log("inputted");
+      this.censoredMessage = "censored";
+    }
+  }
+});
+
+new Vue({
+  el: "#components-6",
+  data: {
+    title: "サンプル"
+  }
+});
+
+new Vue({
+  el: "#components-7",
+  data: {
+    tabsData: [
+      { el: "first-tab", title: "tab 1" },
+      { el: "second-tab", title: "tab TWO" },
+      { el: "third-tab", title: "tab iii" },
+      { el: "fourth-tab", title: "tab Ⅳ" }
+    ],
+    currentTab: "first-tab"
+  },
+  computed: {
+    currentArticleComponent: function() {
+      return this.currentTab;
     }
   }
 });
