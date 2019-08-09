c571b303f86d7a48e0229028232dfce47bd3a90b
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2019-08-09 22:49:42 +0900

    qiita-org-user: Organization内のユーザーをすべて取得できるようになった

--- 20190807-pt27-scraping-qiita-organization-users/scraping_organization_user.py
+++ 20190807-pt27-scraping-qiita-organization-users/scraping_organization_user.py
@@ -1,19 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Organizationsのユーザーでとりあえず1ページ目を取り出す
-o_s = requests.get("https://qiita.com/organizations/opst/members")
+member_id_list = []
+url = "https://qiita.com/organizations/opst/members"
+url_base = "https://qiita.com"
 
-# ページ全体をBeautifulSoup
-o_soup = BeautifulSoup(o_s.text, features="html.parser")
+while True:
+    # Organizationsのユーザーでとりあえず1ページ目を取り出す
+    print(url)
+    o_s = requests.get(url)
 
-# メンバー表示部を選択
-member = o_soup.find("ul", attrs={"class", "p-organization_memberlist"})
+    # ページ全体をBeautifulSoup
+    o_soup = BeautifulSoup(o_s.text, features="html.parser")
 
-# メンバーのリストを取得
-member_list = member.find_all("div", attrs={"class", "od-MemberCardContent"})
+    # メンバー表示部を選択
+    member_soup = o_soup.find("ul", attrs={"class", "p-organization_memberlist"})
 
-# メンバーの名前を出してみる
-for m in member_list:
-    name = m.contents[0].find("strong", attrs={"class", "od-MemberCardHeaderIdentities_username"}).contents
-    print(name)
+    # メンバーのリストを取得
+    member_list_soup = member_soup.find_all("div", attrs={"class", "od-MemberCardContent"})
+
+    # メンバーの名前を出してみる
+    for m in member_list_soup:
+        name = m.contents[0].find("strong", attrs={"class", "od-MemberCardHeaderIdentities_username"}).contents
+        id = m.contents[0].find("span", attrs={"class", "od-MemberCardHeaderIdentities_userid"}).contents[0]
+        member_id_list.append(id.split('@')[1])
+
+    next_pg_soup = o_soup.find("a", attrs={"class": "st-Pager_link", "rel": "next"})
+    if not next_pg_soup:
+        break
+
+    url = url_base + next_pg_soup.get("href")
+
+print(member_id_list)
